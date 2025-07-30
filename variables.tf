d91c2e7b1c07f2e479317f7f956c68e339ed5507
sharmah91 <sharmah91@gmail.com>
2025-07-30 13:49:41 +0530

    Terraform_Variable_vars

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+  description = "name of resource group"
+  type        = string
+}
+
+variable "rg_location" {
+  description = "location of resource group"
+  type        = string
+}
