51680952ec20b22e7e5b47fbfd96ac8e6ba49c7e
sharmah91 <sharmah91@gmail.com>
2025-07-30 13:52:35 +0530

    Terraform_Variable_Vars

--- variables.tf
+++ variables.tf
@@ -7,3 +7,10 @@ variable "rg_location" {
   description = "location of resource group"
   type        = string
 }
+
+
+
+# rg_name     = "rg_hitesh_1"
+# rg_location = "East US"
+
+# These are need to be added in terraform.tfvars file and it is not added in github so that's why I ahve added in this file.
